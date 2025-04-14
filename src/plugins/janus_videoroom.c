326180cda7c9bf603d69a764281787bc54cde4d8
val.v.petrov <val.v.petrov@tinkoff.ru>
2025-04-14 15:37:57 +0300

    also do not reference after decrement

--- src/plugins/janus_videoroom.c
+++ src/plugins/janus_videoroom.c
@@ -2722,6 +2722,7 @@ static void janus_videoroom_publisher_stream_free(const janus_refcount *ps_ref) 
 		/* TODO Anything else we should free? */
 	if(ps->publisher)
 		janus_refcount_decrease(&ps->publisher->ref);
+	ps->publisher = NULL;
 	g_free(ps->mid);
 	g_free(ps->description);
 	g_free(ps->fmtp);
